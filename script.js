cced71b63147a3877a70376760568f7bf2f3da5a
Jack E. Shomer <74559111+jsho982045@users.noreply.github.com>
2024-09-10 01:45:54 +0000

    Changed password

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById("checkBtn").addEventListener("click", function() {
     const userInput = document.getElementById("sentenceInput").value;
     
     const message = document.getElementById("message");
-    
+    4
     if (userInput === correctSentence) {
-        message.innerHTML = "Correct! The password is: <strong>1234</strong>";
+        message.innerHTML = "Correct! The password is: <strong>1324</strong>";
         message.style.color = "green";
     } else {
         message.textContent = "Incorrect sentence. Please try again.";
